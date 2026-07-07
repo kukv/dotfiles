d730ca99cee950c9ffd632a79ef7552899631d35
kukv <koki-nonaka@outlook.jp>
2026-07-08 00:02:16 +0900

    feat(nvim): add html/css/emmet language servers

---
+++ home/dot_config/nvim/lua/plugins/web.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        html = {},
+        cssls = {},
+        emmet_language_server = {},
+      },
+    },
+  },
+}
