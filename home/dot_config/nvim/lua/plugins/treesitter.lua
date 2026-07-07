3684bc1d104df361793fc49961edd5c87e77dce6
kukv <koki-nonaka@outlook.jp>
2026-07-08 00:02:12 +0900

    feat(nvim): ensure tsx and web treesitter parsers

---
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+        "javascript",
+        "html",
+        "css",
+      })
+    end,
+  },
+}
